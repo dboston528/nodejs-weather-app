33b9f9bd65e64f5073a3a4de2a3a84ffe2b5f354
DeAndre Boston <dboston3@yahoo.com>
2021-10-04 21:12:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+I'm building this weather app for practice with Node.js.
+
+I'm following Andrew Mead's course on Udemy. The course is project based, and seems to cover all of the basics for understading Node JS. 
+
+I'll add more to this readme as I progress through this his project. 
+
---
+++ app.js
@@ -0,0 +1,48 @@
+const request = require("postman-request")
+
+// const url = 'http://api.weatherstack.com/current?access_key=1a0524bd57f10eeb53831f17ae333821&query=41.6236346,-87.6609434&units=f';
+
+// request({url: url, json: true}, (error, response)=>{
+    
+//     if(error){
+//         console.log("Unable to connect to weather service!")
+//     } else if (response.body.error){
+//         console.log("Unable to find location")
+        
+//     } else {
+//         const temp = response.body.current.temperature
+//         const feelslike = response.body.current.feelslike
+//         console.log(`It is currently ${temp} degrees out. It feels like ${feelslike} degrees out.`)
+//     }
+
+
+    
+// })
+
+
+const map_box_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/ioiggghj.json?access_token=pk.eyJ1IjoiZGJvc3RvbjUyOCIsImEiOiJja3UzajFpcHkxMG84Mm9uaWRoeDg5NHZwIn0.66HPrpLNABaU0FqZ1nypag&limit=1'
+
+request({url: map_box_url , json: true}, (error, response)=>{
+    if(error){
+        console.log("You aint connected bro!")
+    } else if (response.body.error){
+        console.log('we caint find dat location, sorry')
+
+    }else {
+        const lon = response.body.features[0].center[0]
+        const lat = response.body.features[0].center[1]
+        console.log(lat,lon)
+
+    }
+
+    
+
+    
+}) 
+
+//Goal: Handle errors for geocoding request
+
+// 1. Setup an error for low-level errors
+// 2. Test by disabling network request and running the app
+// 3. setup error handling for no matchin the app`
+// 4. test by altering the search term and running the app. 
